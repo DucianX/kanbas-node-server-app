360b912d81789ccc589d122ac9966f845dcd19d9
xuyit <xu.yit@northeastern.edu>
2024-11-21 19:17:09 -0800

    done all

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -3,3 +3,16 @@ export function enrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   enrollments.push({ _id: Date.now().toString(), user: userId.toString(), course: courseId.toString() });
 }
+
+export function deleteEnrollment(userId, courseId) {
+  const { enrollments } = Database;
+  Database.enrollments = enrollments.filter(
+    // filter=>后面的是符合留下来的条件的元素
+    (enrollment) => enrollment.user!== userId || enrollment.course!== courseId
+  );
+}
+
+export function fetchAllEnrollment() {
+  const { enrollments } = Database;
+  return enrollments
+}
--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -5,9 +5,22 @@ export default function EnrollmentRoutes(app) {
 
     const {courseId} = req.params;
     const {userId} = req.body;
-    console.log("Received courseId:", courseId, "userId:", userId);
 
     dao.enrollUserInCourse(userId, courseId);
     res.send("Enrollment created successfully");
+  })
+
+  app.post("/api/courses/:courseId/deleteEnrollment", (req, res) => {
+
+    const {courseId} = req.params;
+    const {userId} = req.body;
+
+    dao.deleteEnrollment(userId, courseId);
+    res.send("Enrollment deleted successfully");
+  })
+
+  app.get("/api/courses/getEnrollments", (req, res) => {
+    const enrollments = dao.fetchAllEnrollment();
+    res.json(enrollments);
   })
 }
