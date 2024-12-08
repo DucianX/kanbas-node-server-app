17f95f46077dc28ae50d1070b7737bf301c6349e
xuyit <xu.yit@northeastern.edu>
2024-12-07 22:06:15 -0800

    done user filtering by role

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,17 +1,32 @@
 // 本文件把对数据库的操作进行集中封装（高内聚High Cohesion：这个模块的功能紧密相关），这样在调用的时候就不用关心具体数据库的选型
 // 若要切换数据库，只需要更改 dao.js 中的逻辑，而不需要修改与之交互的其他部分。（低耦合Low Coupling）
 // 它的目的是让其他部分的代码（如服务或控制器）与数据存储层进行隔离，使得更改底层数据库的实现不会影响到应用程序的其他部分。
-import db from "../Database/index.js";
 
 // 从index.js导入db，并且赋值为users
-let { users } = db;
-export const createUser = (user) => (users = [...users, { ...user, _id: Date.now().toString() }]);
-export const findAllUsers = () => users;
-export const findUserById = (userId) => users.find((user) => user._id === userId);
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
-export const findUserByCredentials = (username, password) =>
-  users.find( (user) => user.username === username && user.password === password );
-// 遍历，直到id符合条件的时候把对应的user直接换成传入的user数据
-export const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+// let { users } = db;
+// export const createUser = (user) => (users = [...users, { ...user, _id: Date.now().toString() }]);
+// export const findAllUsers = () => users;
+// export const findUserById = (userId) => users.find((user) => user._id === userId);
+// export const findUserByUsername = (username) => users.find((user) => user.username === username);
+// export const findUserByCredentials = (username, password) =>
+//   users.find( (user) => user.username === username && user.password === password );
+// // 遍历，直到id符合条件的时候把对应的user直接换成传入的user数据
+// export const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
+// export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+//
+import model from "./model.js";
+
+export const createUser = (user) => {
+  delete user._id;
+  return model.create(user);
+} // implemented later
+export const findAllUsers = () => model.find(); // 相当于select * from users
+export const findUserById = (userId) => model.findById(userId); // 用primaryKey寻找到user
+export const findUserByUsername = (username) =>  model.findOne({ username: username }); // 用name和value来过滤model 在javaScript里面可一所写成username
+// 当变量和属性名字相同，可以省略:...
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user)  =>  model.updateOne({ _id: userId }, { $set: user }); // set提供了想要修改的field和修改的值，只有不同的时候才会操作更改
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+// findOne唯一匹配，find返回所有匹配的item为一个数组
+export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
 
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -6,43 +6,55 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 // let currentUser = null;
 export default function UserRoutes(app) {
+  // 因为涉及到了服务器的返回，我们需要把所有的函数设置为异步函数
   const createUser = (req, res) => {
   };
   const deleteUser = (req, res) => {
   };
-  const findAllUsers = (req, res) => {
-  };
-  const findUserById = (req, res) => {
+  const findAllUsers = async (req, res) => {
+    // 如果还包含了requery，那么进行过滤
+    const { role } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return; // 跳过下面的response
+    }
+
+    const users = await dao.findAllUsers();
+    res.json(users);
   };
 
-  const updateUser = (req, res) => {
+  const findUserById = (req, res) => {
+  };
+// 和数据库的交流需要用async异步函数，await等待回传信息
+  const updateUser = async (req, res) => {
     const userId = req.params.userId;
     const userUpdates = req.body;
     // 将需要更新的部分放在body里面一起传递给dao内的update方法
-    dao.updateUser(userId, userUpdates);
+    await dao.updateUser(userId, userUpdates);
     // 一旦完成，我将用id来获得新的user，并且返回
-    const currentUser = dao.findUserById(userId);
+    const currentUser = await dao.findUserById(userId);
     // 同步更新session里的user
     req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
-  const signup = (req, res) => {
+  const signup = async(req, res) => {
     // 相信函数定义
     // 首先检查用户是否已经存在。
-    const user = dao.findUserByUsername(req.body.username);
+    const user = await dao.findUserByUsername(req.body.username);
     if (user) {
       res.status(400).json(
         {message: "Username already in use"});
       return;
     }
-    const currentUser = dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
     // 每一个不同的cookie将会有不同的session，每一个session里像hash一样记录下不同的currentUser（当前登录用户）
     req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
-  const signin = (req, res) => {
+  const signin = async(req, res) => {
     const {username, password} = req.body;
-    const currentUser = dao.findUserByCredentials(username, password);
+    const currentUser = await dao.findUserByCredentials(username, password);
     if (currentUser) {
       // 如果能找到，就在session里面记录当前的user
       req.session["currentUser"] = currentUser;
@@ -52,7 +64,7 @@ export default function UserRoutes(app) {
     }
 
   };
-  const signout = (req, res) => {
+  const signout = async (req, res) => {
     // 通过摧毁当前session来登出
     req.session.destroy();
     res.sendStatus(200);
@@ -71,7 +83,7 @@ export default function UserRoutes(app) {
 
     res.json(currentUser);
   };
-  const findCoursesForEnrolledUser = (req, res) => {
+  const findCoursesForEnrolledUser = async(req, res) => {
     let {userId} = req.params;
     if (userId === "current") {
       const currentUser = req.session["currentUser"];
@@ -81,15 +93,15 @@ export default function UserRoutes(app) {
       }
       userId = currentUser._id;
     }
-    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    const courses = await courseDao.findCoursesForEnrolledUser(userId);
     res.json(courses);
   };
   // 因为希望确保创始人能够访问到课程，所以创建课程在users的上下文中进行
   // 课程定的标准：优先迁就user
-  const createCourse = (req, res) => {
+  const createCourse = async(req, res) => {
     const currentUser = req.session["currentUser"];
-    const newCourse = courseDao.createCourse(req.body);
-    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    const newCourse = await courseDao.createCourse(req.body);
+    await enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
     res.json(newCourse);
   };
 
